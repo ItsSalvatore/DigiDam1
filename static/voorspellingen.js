ff4881a8a2a7d8d2375ab8effdaf5b692174baf7
Salvatore <mano.schaap@hva.nl>
2023-06-01 01:59:33 +0200

    vvvv

---
+++ static/voorspellingen.js
@@ -0,0 +1,47 @@
+
+
+
+// VOORSPELLINGEN VOOR SCRUMMASTERS, 2024-2027
+var scrmmstr = {
+    scr2023: "31",
+    scr2024: "35",
+    scr2025: "40",
+    scr2026: "45",
+    scr2027: "50"
+};
+// VOORSPELLINGEN VOOR AGILE COACHES, 2024-2027
+var ac = {
+    ac2023: "31",
+    ac2024: "35",
+    ac2025: "40",
+    ac2026: "45",
+    ac2027: "50"
+};// VOORSPELLINGEN VOOR PRODUCT OWNERS, 2024-2027
+var po = {
+    po2023: "31",
+    po2024: "35",
+    po2025: "40",
+    po2026: "45",
+    po2027: "50"
+};// VOORSPELLINGEN VOOR RTE's, 2024-2027
+var rte = {
+    rte2023: "31",
+    rte2024: "35",
+    rte2025: "40",
+    rte2026: "45",
+    rte2027: "50"
+};// VOORSPELLINGEN VOOR PROJECT MANAGERS, 2024-2027
+var pjmgr = {
+    pjmgr2023: "31",
+    pjmgr2024: "35",
+    pjmgr2025: "40",
+    pjmgr2026: "45",
+    pjmgr2027: "50"
+};// VOORSPELLINGEN VOOR TOTAAL AANTAL ROLLEN
+var totaal = {
+    totaal2023: "166",
+    totaal2024: "175",
+    totaal2025: "183",
+    totaal2026: "190",
+    totaal2027: "201"
+}
