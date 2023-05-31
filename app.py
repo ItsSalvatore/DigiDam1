0909712f6d84c845f0229c44844a972d1dd73af0
Salvatore <mano.schaap@hva.nl>
2023-06-01 01:18:09 +0200

    mmm

--- app.py
+++ app.py
@@ -108,7 +108,28 @@ def view_scrummasters():
 
 
     }
-    print(totaalRollen)
+    opdrachten = get_allopdracht()
+    count_list = {}
+    # kijk naar alle opdrachten
+    for opdracht in opdrachten:
+        year1 = opdracht[3].year
+        # check of begindatum/beginjaar al in de dictonary zit
+        if year1 not in count_list:
+            # zo nee, voeg toe
+            count_list[year1] = 1
+        else: 
+            # zo ja, verhoog waarde met 1
+            count_list[year1] = count_list[year1] + 1
+
+    for opdracht in opdrachten: 
+        year2 = opdracht[4].year
+        # check of begindatum/beginjaar al in de dictonary zit
+        if year2 not in count_list:
+            # zo nee, voeg toe
+            count_list[year2] = 1
+        else: 
+            # zo ja, verhoog waarde met 1
+            count_list[year2] = count_list[year2] + 1
     scrummasters = get_scrummasters()
     agilecoaches = get_agilecoaches()
     productowners = get_productowners()
@@ -118,7 +139,19 @@ def view_scrummasters():
     pmo = get_pmo()
     pgm = get_pgm()
 
-    return render_template('index.html',totaalRollen=totaalRollen, agilecoaches=agilecoaches, productowners=productowners, rte=rte, ate=ate, pjm=pjm, pmo=pmo, pgm=pgm, data=data, scrummasters=scrummasters)
+
+
+    scrummasters_2024 = get_scrummasters_2024()
+    agilecoaches_2024 = get_agilecoaches_2024()
+    productowners_2024 = get_productowners_2024()
+    RTE_2024 = get_RTE_2024()
+    ATE_2024 = get_ATE_2024()
+    projectmanager_2024 = get_projectmanagers_2024()
+    PMO_2024 = get_PMO_2024()
+    PGM_2024 = get_PGM_2024()
+    productowners_2023 = get_productowners_2023
+
+    return render_template('index.html',opdrachten=count_list, totaalRollen=totaalRollen, agilecoaches=agilecoaches, productowners=productowners, rte=rte, ate=ate, pjm=pjm, pmo=pmo, pgm=pgm, productowners_2023=productowners_2023, data=data, scrummasters=scrummasters, scrummasters_2024=scrummasters_2024, agilecoaches_2024=agilecoaches_2024,productowners_2024=productowners_2024,RTE_2019=RTE_2024,ATE_2024=ATE_2024,projectmanager_2024=projectmanager_2024,PMO_2024=PMO_2024,PGM_2024=PGM_2024)
 
 # definitieve test.
 
@@ -234,9 +267,142 @@ def get_pmo():
     result = db.session.execute(query)
     return result.all()
 
-# route voor de indexpagina
 
-# eind definitieve test.
+
+
+
+
+
+
+
+# functie om het aantal scrummasters in 2024 op te halen 
+def get_scrummasters_2024():
+    query = """SELECT COUNT(*)
+               FROM medewerkers
+               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
+               AND id IN (SELECT medewerker_id
+                           FROM medewerkers_has_rol
+                           WHERE rol_id = (SELECT id
+                                           FROM rol
+                                           WHERE rolnaam = 'SM'))"""
+    
+    result = db.session.execute(query)
+    return result.scalar()
+
+# functie om het aantal agile coaches in 2019 te tellen
+def get_agilecoaches_2024():
+    query = """SELECT COUNT(*)
+               FROM medewerkers
+               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
+               AND id IN (SELECT medewerker_id
+                           FROM medewerkers_has_rol
+                           WHERE rol_id = (SELECT id
+                                           FROM rol
+                                           WHERE rolnaam = 'AC'))"""
+    
+    result = db.session.execute(query)
+    return result.scalar()
+
+# functie om het aantal product owners in 2019 te tellen
+def get_productowners_2024():
+    query = """SELECT COUNT(*)
+               FROM medewerkers
+               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
+               AND id IN (SELECT medewerker_id
+                          FROM medewerkers_has_rol
+                          WHERE rol_id = (SELECT id
+                                          FROM rol
+                                          WHERE rolnaam = 'PO'))"""
+    result = db.session.execute(query)
+    return result.scalar()
+    
+# functie om het aantal RTE in 2019 op te halen
+def get_RTE_2024():
+    query = """SELECT COUNT(*)
+               FROM medewerkers
+               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
+               AND id IN (SELECT medewerker_id
+                          FROM medewerkers_has_rol
+                          WHERE rol_id = (SELECT id
+                                          FROM rol
+                                          WHERE rolnaam = 'RTE'))"""
+    result = db.session.execute(query)
+    return result.scalar()
+
+#  functie om het aantal ATE in 2019 op te halen
+def get_ATE_2024():
+    query = """SELECT COUNT(*)
+               FROM medewerkers
+               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
+               AND id IN (SELECT medewerker_id
+                          FROM medewerkers_has_rol
+                          WHERE rol_id = (SELECT id
+                                          FROM rol
+                                          WHERE rolnaam = 'ATE'))"""
+    result = db.session.execute(query)
+    return result.scalar()
+
+# functie om het aantal project managers in 2019 op te halen
+def get_projectmanagers_2024():
+    query = """SELECT COUNT(*)
+               FROM medewerkers
+               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
+               AND id IN (SELECT medewerker_id
+                          FROM medewerkers_has_rol
+                          WHERE rol_id = (SELECT id
+                                          FROM rol
+                                          WHERE rolnaam = 'PJM'))"""
+    result = db.session.execute(query)
+    return result.scalar()
+
+# functie om het aantal PMO in 2019 op te halen
+def get_PMO_2024():
+    query = """SELECT COUNT(*)
+               FROM medewerkers
+               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
+               AND id IN (SELECT medewerker_id
+                          FROM medewerkers_has_rol
+                          WHERE rol_id = (SELECT id
+                                          FROM rol
+                                          WHERE rolnaam = 'PMO'))"""
+    result = db.session.execute(query)
+    return result.scalar()
+
+# functie om het aantal PGM in 2019 op te halen
+def get_PGM_2024():
+    query = """SELECT COUNT(*)
+               FROM medewerkers
+               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
+               AND id IN (SELECT medewerker_id
+                          FROM medewerkers_has_rol
+                          WHERE rol_id = (SELECT id
+                                          FROM rol
+                                          WHERE rolnaam = 'PGM'))"""
+    result = db.session.execute(query)
+    return result.scalar()
+
+
+def get_productowners_2023():
+    query = """SELECT COUNT(*)
+               FROM medewerkers
+               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2023-12-31'
+               AND id IN (SELECT medewerker_id
+                          FROM medewerkers_has_rol
+                          WHERE rol_id = (SELECT id
+                                          FROM rol
+                                          WHERE rolnaam = 'PO'))"""
+    result = db.session.execute(query)
+    return result.scalar()
+
+def get_allopdracht():
+    query = """SELECT *
+               FROM opdracht
+               WHERE einddatum BETWEEN '2020-01-01' AND '2027-12-31' 
+               AND startdatum BETWEEN '2020-01-01' AND '2027-12-31'
+               ORDER BY startdatum, einddatum
+             """
+    result = db.session.execute(query)
+    return result.all()
 
 
 @app.route('/users')
@@ -263,3 +429,7 @@ def view_voorspellingen():
         "totaal" : Medewerkers_rol.query.count()
     }
     return render_template('voorspellingen.html', data = data)
+
+@app.route('/handleiding')
+def view_handleiding():
+    return render_template('handleiding.html')
