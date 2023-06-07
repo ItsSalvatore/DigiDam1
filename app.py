ec94e2e53723854c28ce6096d1e7c54e3877e26a
Owent01 <owentren1@outlook.com>
2023-06-07 20:27:56 +0200

    Nieuwe versie, bijna complete applicatie

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+'''
+06-06-2023
+Team Digidam
+
+Beschrijving app.py:
+
+
+'''
+
+
 from flask import Flask, render_template, request, jsonify
-from models import db, Medewerkers_rol
+from models import db, Medewerkers_rol, Medewerkers
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -24,93 +34,10 @@ def view_scrummasters():
         "ate" : Medewerkers_rol.query.filter_by(rol_id='6').count(),
         "pjm" : Medewerkers_rol.query.filter_by(rol_id='7').count(),
         "pmo" : Medewerkers_rol.query.filter_by(rol_id='8').count(),
-        "totaal" : Medewerkers_rol.query.count()
+        "totaal" : Medewerkers_rol.query.count(),
+        "totaalmedewerkers": Medewerkers.query.count()
     }
 
-    totaalRollen = {
-        "smTotal" : {
-            2020: 35,
-            2021: 40,
-            2022: 50,
-            2023: 42,
-            2024: 35,
-            2025: 40,
-            2026: 45,
-            2027: 45,
-        },
-        "acTotal" : {
-            2020: 2,
-            2021: 3,
-            2022: 2,
-            2023: 4,
-            2024: 5,
-            2025: 10,
-            2026: 15,
-            2027: 15,
-        },
-        "poTotal" : {
-            2020: 10,
-            2021: 10,
-            2022: 10,
-            2023: 10,
-            2024: 10,
-            2025: 10,
-            2026: 10,
-            2027: 10,
-        },
-        "rteTotal" : {
-            2020: 4,
-            2021: 4,
-            2022: 4,
-            2023: 4,
-            2024: 4,
-            2025: 4,
-            2026: 4,
-            2027: 4,
-        },
-        "ateTotal" : {
-            2020: 7,
-            2021: 7,
-            2022: 7,
-            2023: 7,
-            2024: 7,
-            2025: 7,
-            2026: 7,
-            2027: 7,
-        },
-        "pjmTotal" : {
-            2020: 200,
-            2021: 150,
-            2022: 130,
-            2023: 100,
-            2024: 110,
-            2025: 90,
-            2026: 70,
-            2027: 60,
-        },
-        "pmoTotal" : {
-            2020: 11,
-            2021: 11,
-            2022: 11,
-            2023: 11,
-            2024: 11,
-            2025: 11,
-            2026: 11,
-            2027: 11,
-        },
-        "pgmTotal" : {
-            2020: 12,
-            2021: 12,
-            2022: 13,
-            2023: 14,
-            2024: 12,
-            2025: 12,
-            2026: 12,
-            2027: 12,
-        }
-
-
-    }
     opdrachten = get_allopdracht()
     count_list = {}
     # kijk naar alle opdrachten
@@ -124,15 +51,17 @@ def view_scrummasters():
             # zo ja, verhoog waarde met 1
             count_list[year1] = count_list[year1] + 1
 
-    for opdracht in opdrachten: 
-        year2 = opdracht[4].year
-        # check of begindatum/beginjaar al in de dictonary zit
-        if year2 not in count_list:
-            # zo nee, voeg toe
-            count_list[year2] = 1
-        else: 
-            # zo ja, verhoog waarde met 1
-            count_list[year2] = count_list[year2] + 1
+    # for opdracht in opdrachten: 
+    #     year2 = opdracht[4].year
+    #     # check of begindatum/beginjaar al in de dictonary zit
+    #     if year2 not in count_list:
+    #         # zo nee, voeg toe
+    #         count_list[year2] = 1
+    #     else: 
+    #         # zo ja, verhoog waarde met 1
+    #         count_list[year2] = count_list[year2] + 1
+            
+    # Krijg alle rollen vanuit database
     scrummasters = get_scrummasters()
     agilecoaches = get_agilecoaches()
     productowners = get_productowners()
@@ -142,19 +71,7 @@ def view_scrummasters():
     pmo = get_pmo()
     pgm = get_pgm()
 
-
-
-    scrummasters_2024 = get_scrummasters_2024()
-    agilecoaches_2024 = get_agilecoaches_2024()
-    productowners_2024 = get_productowners_2024()
-    RTE_2024 = get_RTE_2024()
-    ATE_2024 = get_ATE_2024()
-    projectmanager_2024 = get_projectmanagers_2024()
-    PMO_2024 = get_PMO_2024()
-    PGM_2024 = get_PGM_2024()
-    productowners_2023 = get_productowners_2023
-
-    return render_template('index.html', opdrachten=count_list, totaalRollen=totaalRollen, agilecoaches=agilecoaches, productowners=productowners, rte=rte, ate=ate, pjm=pjm, pmo=pmo, pgm=pgm, productowners_2023=productowners_2023, data=data, scrummasters=scrummasters, scrummasters_2024=scrummasters_2024, agilecoaches_2024=agilecoaches_2024,productowners_2024=productowners_2024,RTE_2019=RTE_2024,ATE_2024=ATE_2024,projectmanager_2024=projectmanager_2024,PMO_2024=PMO_2024,PGM_2024=PGM_2024)
+    return render_template('index.html', opdrachten=count_list, agilecoaches=agilecoaches, productowners=productowners, rte=rte, ate=ate, pjm=pjm, pmo=pmo, pgm=pgm, data=data, scrummasters=scrummasters, )
 
 # definitieve test.
 
@@ -271,132 +188,7 @@ def get_pmo():
     return result.all()
 
 
-
-
-
-
-
-
-
-# functie om het aantal scrummasters in 2024 op te halen 
-def get_scrummasters_2024():
-    query = """SELECT COUNT(*)
-               FROM medewerkers
-               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
-               AND id IN (SELECT medewerker_id
-                           FROM medewerkers_has_rol
-                           WHERE rol_id = (SELECT id
-                                           FROM rol
-                                           WHERE rolnaam = 'SM'))"""
-    
-    result = db.session.execute(query)
-    return result.scalar()
-
-# functie om het aantal agile coaches in 2019 te tellen
-def get_agilecoaches_2024():
-    query = """SELECT COUNT(*)
-               FROM medewerkers
-               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
-               AND id IN (SELECT medewerker_id
-                           FROM medewerkers_has_rol
-                           WHERE rol_id = (SELECT id
-                                           FROM rol
-                                           WHERE rolnaam = 'AC'))"""
-    
-    result = db.session.execute(query)
-    return result.scalar()
-
-# functie om het aantal product owners in 2019 te tellen
-def get_productowners_2024():
-    query = """SELECT COUNT(*)
-               FROM medewerkers
-               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
-               AND id IN (SELECT medewerker_id
-                          FROM medewerkers_has_rol
-                          WHERE rol_id = (SELECT id
-                                          FROM rol
-                                          WHERE rolnaam = 'PO'))"""
-    result = db.session.execute(query)
-    return result.scalar()
-    
-# functie om het aantal RTE in 2019 op te halen
-def get_RTE_2024():
-    query = """SELECT COUNT(*)
-               FROM medewerkers
-               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
-               AND id IN (SELECT medewerker_id
-                          FROM medewerkers_has_rol
-                          WHERE rol_id = (SELECT id
-                                          FROM rol
-                                          WHERE rolnaam = 'RTE'))"""
-    result = db.session.execute(query)
-    return result.scalar()
-
-#  functie om het aantal ATE in 2019 op te halen
-def get_ATE_2024():
-    query = """SELECT COUNT(*)
-               FROM medewerkers
-               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
-               AND id IN (SELECT medewerker_id
-                          FROM medewerkers_has_rol
-                          WHERE rol_id = (SELECT id
-                                          FROM rol
-                                          WHERE rolnaam = 'ATE'))"""
-    result = db.session.execute(query)
-    return result.scalar()
-
-# functie om het aantal project managers in 2019 op te halen
-def get_projectmanagers_2024():
-    query = """SELECT COUNT(*)
-               FROM medewerkers
-               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
-               AND id IN (SELECT medewerker_id
-                          FROM medewerkers_has_rol
-                          WHERE rol_id = (SELECT id
-                                          FROM rol
-                                          WHERE rolnaam = 'PJM'))"""
-    result = db.session.execute(query)
-    return result.scalar()
-
-# functie om het aantal PMO in 2019 op te halen
-def get_PMO_2024():
-    query = """SELECT COUNT(*)
-               FROM medewerkers
-               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
-               AND id IN (SELECT medewerker_id
-                          FROM medewerkers_has_rol
-                          WHERE rol_id = (SELECT id
-                                          FROM rol
-                                          WHERE rolnaam = 'PMO'))"""
-    result = db.session.execute(query)
-    return result.scalar()
-
-# functie om het aantal PGM in 2019 op te halen
-def get_PGM_2024():
-    query = """SELECT COUNT(*)
-               FROM medewerkers
-               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2024-12-31'
-               AND id IN (SELECT medewerker_id
-                          FROM medewerkers_has_rol
-                          WHERE rol_id = (SELECT id
-                                          FROM rol
-                                          WHERE rolnaam = 'PGM'))"""
-    result = db.session.execute(query)
-    return result.scalar()
-
-
-def get_productowners_2023():
-    query = """SELECT COUNT(*)
-               FROM medewerkers
-               WHERE einddatum_contract BETWEEN '1980-01-01' AND '2023-12-31'
-               AND id IN (SELECT medewerker_id
-                          FROM medewerkers_has_rol
-                          WHERE rol_id = (SELECT id
-                                          FROM rol
-                                          WHERE rolnaam = 'PO'))"""
-    result = db.session.execute(query)
-    return result.scalar()
-
+# functie om alle opdrachten ooit uit de database te halen
 def get_allopdracht():
     query = """SELECT *
                FROM opdracht
@@ -407,7 +199,7 @@ def get_allopdracht():
     result = db.session.execute(query)
     return result.all()
 
-
+# Voorspellingen filter query
 @app.route('/voorspellingen')
 def view_voorspellingen():
     data = {
@@ -419,10 +211,12 @@ def view_voorspellingen():
         "ate" : Medewerkers_rol.query.filter_by(rol_id='6').count(),
         "pjm" : Medewerkers_rol.query.filter_by(rol_id='7').count(),
         "pmo" : Medewerkers_rol.query.filter_by(rol_id='8').count(),
-        "totaal" : Medewerkers_rol.query.count()
+        "totaal" : Medewerkers_rol.query.count(),
+        "totaalmedewerkers": Medewerkers.query.count()
     }
     return render_template('voorspellingen.html', data = data)
 
+# handleiding pagina
 @app.route('/handleiding')
 def view_handleiding():
     return render_template('handleiding.html')
--- static/voorspellingen.js
+++ static/voorspellingen.js
@@ -3,6 +3,9 @@
 
 // VOORSPELLINGEN VOOR SCRUMMASTERS, 2024-2027
 var scrmmstr = {
+    scr2020: "35",
+    scr2021: "40",
+    scr2022: "50",
     scr2023: "42",
     scr2024: "57",
     scr2025: "83",
@@ -11,27 +14,30 @@ var scrmmstr = {
 };
 // VOORSPELLINGEN VOOR AGILE COACHES, 2024-2027
 var ac = {
-    ac2023: "15",
-    ac2024: "21",
-    ac2025: "32",
-    ac2026: "41",
-    ac2027: "50"
+    ac2023: "13",
+    ac2024: "16",
+    ac2025: "24",
+    ac2026: "30",
+    ac2027: "36"
 };// VOORSPELLINGEN VOOR PRODUCT OWNERS, 2024-2027
 var po = {
-    po2023: "31",
-    po2024: "35",
-    po2025: "40",
-    po2026: "45",
-    po2027: "50"
+    po2023: "42",
+    po2024: "57",
+    po2025: "83",
+    po2026: "105",
+    po2027: "127"
 };// VOORSPELLINGEN VOOR PROGRAMMA MANAGERS, 2024-2027
 var pgm = {
-    pgm2023: "31",
-    pgm2024: "35",
-    pgm2025: "40",
-    pgm2026: "45",
-    pgm2027: "50"
+    pgm2023: "3",
+    pgm2024: "3",
+    pgm2025: "3",
+    pgm2026: "3",
+    pgm2027: "3"
 };// VOORSPELLINGEN VOOR RTE's, 2024-2027
 var rte = {
+    rte2020: "4",
+    rte2021: "4",
+    rte2022: "4",
     rte2023: "4",
     rte2024: "6",
     rte2025: "8",
@@ -39,30 +45,30 @@ var rte = {
     rte2027: "14"
 };// VOORSPELLINGEN VOOR ATE's, 2024-2027
 var ate = {
-    ate2023: "31",
-    ate2024: "35",
-    ate2025: "40",
-    ate2026: "45",
-    ate2027: "50"
+    ate2023: "9",
+    ate2024: "9",
+    ate2025: "8",
+    ate2026: "8",
+    ate2027: "7"
 };// VOORSPELLINGEN VOOR PROJECT MANAGERS, 2024-2027
 var pjmgr = {
-    pjmgr2023: "31",
-    pjmgr2024: "35",
-    pjmgr2025: "40",
-    pjmgr2026: "45",
-    pjmgr2027: "50"
+    pjmgr2023: "73",
+    pjmgr2024: "70",
+    pjmgr2025: "65",
+    pjmgr2026: "60",
+    pjmgr2027: "55"
 };// VOORSPELLINGEN VOOR PMO's, 2024-2027
 var pmo = {
-    pmo2023: "31",
-    pmo2024: "35",
-    pmo2025: "40",
-    pmo2026: "45",
-    pmo2027: "50"
+    pmo2023: "11",
+    pmo2024: "10",
+    pmo2025: "9",
+    pmo2026: "8",
+    pmo2027: "7"
 };// VOORSPELLINGEN VOOR TOTAAL AANTAL ROLLEN
 var totaal = {
-    totaal2023: "166",
-    totaal2024: "175",
-    totaal2025: "183",
-    totaal2026: "190",
-    totaal2027: "201"
+    totaal2023: "161",
+    totaal2024: "178",
+    totaal2025: "208",
+    totaal2026: "234",
+    totaal2027: "259"
 }
